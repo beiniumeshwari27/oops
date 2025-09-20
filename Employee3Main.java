309bbdcbcff4c15fb9343b1defb90d1be335ca72
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-09-20 09:03:25 +0530

    javafullstack

---
+++ Employee3Main.java
@@ -0,0 +1,19 @@
+package com.codegnan.oopexamples;
+
+public class Employee3Main {
+
+	public static void main(String[] args) {
+				Employee3 e1=new Employee3(124,"Amrutha",20000);
+		     e1.DisplayInfo();
+		     e1.setEmployeeId(156);
+		     e1.setName("Arun");
+		     e1.setSalary(-40000);
+			e1.DisplayInfo();
+			}
+
+		}
+
+	
+
+
+
