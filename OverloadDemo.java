552f12cf60d248f7382579f78a74a54bc2e31b39
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-09-20 09:18:49 +0530

    javafullstack

---
+++ OverloadDemo.java
@@ -0,0 +1,21 @@
+package com.codegnan.oopexamples;
+
+public class OverloadDemo {
+	public void test() {
+		System.out.println("no parameters");
+		
+	}
+	public void test(int a) {
+		System.out.println("a:"+a);
+	}
+	public void test(int a,int b) {
+		System.out.println("A and B is:"+a+":"+b);
+		
+	}
+	public double test(double a) {
+		System.out.println("double a:"+a);
+		return a*a;
+		
+	}
+
+}
