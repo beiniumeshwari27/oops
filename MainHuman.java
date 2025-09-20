5149818848b36b0dc29630dbc775229088e5a814
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-09-20 09:14:47 +0530

    javafullstack

---
+++ MainHuman.java
@@ -0,0 +1,16 @@
+package com.codegnan.oopexamples;
+
+public class MainHuman {
+
+
+		public static void main(String[] args) {
+			    Human human1 = new Human();
+				human1.setName("Umeshwari");
+				human1.setAge(21);
+				System.out.println("Name : " +human1.getName()  +  " age : " +human1.getAge());
+		}
+
+	}
+
+
+
