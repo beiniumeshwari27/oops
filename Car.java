a536d9d60e4df46202aa44bb5834b051199159d8
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-09-20 09:01:41 +0530

    javafullstack

---
+++ Car.java
@@ -0,0 +1,24 @@
+package com.codegnan.oopexamples;
+
+public class Car {
+	
+		String Model;
+		 int year;
+		 
+		 public void setModel(String Model) {
+			 this.Model=Model;
+		 }
+		 
+		 public String getModel() {
+			 return Model;
+		 }
+		 public void setYear(int year) {
+			 this.year=year;
+			 }
+		 public int getyear() {
+			 return year;
+		 }
+
+	}
+
+
