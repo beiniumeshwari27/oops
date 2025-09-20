90bdf1039beab39d7a5c0a55ddb3dd9a78ad17d3
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-09-20 09:07:17 +0530

    javafullstack

---
+++ Human.java
@@ -0,0 +1,22 @@
+package com.codegnan.oopexamples;
+
+public class Human {
+		 String name;
+		 int age;
+		 
+		 public void setName(String name) {
+			 this.name=name;
+		 }
+		 
+		 public String getName() {
+			 return name;
+		 }
+		 public void setAge(int age) {
+			 this.age=age;
+			 }
+		 public int getAge() {
+			 return age;
+		 }
+}
+
+
