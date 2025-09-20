f125bb4d93cdafb3b5e55180e93ea13677241372
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-09-20 08:58:43 +0530

    javafullstack

---
+++ Main_01.java
@@ -0,0 +1,16 @@
+package com.codegnan.oopexamples;
+
+public class Main_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Employee e1=new Employee();
+		//e1.displayInfo();
+		//Employee e2=new Employee(101,"uma");
+		//e2.displayInfo();
+		Employee e3=new Employee(102,"preethi","training");
+				e3.displayInfo();
+
+	}
+
+}
